d5379d2129c5ee225324bbf7651904a858f84181
Arnel <nelly02d@gmail.com>
2023-04-26 23:40:24 -0400

    Add function for the button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog'
+import { InventoryFormComponent } from './inventory-form/inventory-form.component';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inventory-app';
+
+  constructor(private _dialog: MatDialog) {}
+
+  openAddInventoryForm() {
+    this._dialog.open(InventoryFormComponent)
+  }
 }
