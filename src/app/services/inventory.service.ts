5467f1c4efb447d9025d5fe6b0600bec53cc4471
Arnel <nelly02d@gmail.com>
2023-04-27 01:27:38 -0400

    Add get services to get data from database to html

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -10,6 +10,10 @@ export class InventoryService {
   constructor(private _http: HttpClient) { }
 
   addInventory(data: any): Observable<any> {
-    return this._http.post('http://localhost:3000/inventory', data) //this will hold the inventory list
+    return this._http.post('http://localhost:3000/inventory', data) //this will push the inventory list to fake database
+  }
+
+  getInventoryList(): Observable<any> {
+    return this._http.get('http://localhost:3000/inventory') //this will get the inventory list from fake database
   }
 }
