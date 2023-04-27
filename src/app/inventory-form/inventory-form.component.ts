0dd503479fde66c5db7c0a37205747052491aa0f
Arnel <nelly02d@gmail.com>
2023-04-27 00:34:12 -0400

    Finished styling and modifying the form

--- src/app/inventory-form/inventory-form.component.ts
+++ src/app/inventory-form/inventory-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { InventoryService } from '../services/inventory.service';
 
 @Component({
   selector: 'app-inventory-form',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./inventory-form.component.scss']
 })
 export class InventoryFormComponent {
+  invForm: FormGroup
 
+  constructor(private _fb: FormBuilder, private _invService: InventoryService ) { //service variable 
+    this.invForm = this._fb.group({
+      category: '',
+      input: '',
+      quantity: '',
+      datePurchase: '',
+    })
+  }
+
+  onFormSubmit() {
+    if(this.invForm.valid) {
+      this._invService.addInventory(this.invForm.value).subscribe({ //inject the inventory form value to inventory service
+
+      })
+    }
+  }
 }
