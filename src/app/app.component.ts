84f486d2fe01f5eedc7498492b4b7f8f853b6a09
Arnel <nelly02d@gmail.com>
2023-04-27 01:31:33 -0400

    Add get services and get function to view data on html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog'
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
 import { InventoryFormComponent } from './inventory-form/inventory-form.component';
+import { InventoryService } from './services/inventory.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 
 @Component({
@@ -8,12 +12,48 @@ import { InventoryFormComponent } from './inventory-form/inventory-form.componen
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'inventory-app';
+export class AppComponent implements OnInit{
+  displayedColumns: string[] = [
+    'id',
+    'category', 
+    'input', 
+    'quantity', 
+    'datePurchase'
+  ];
 
-  constructor(private _dialog: MatDialog) {}
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private _dialog: MatDialog, private _invService: InventoryService) {}
+
+  ngOnInit(): void {
+   this.getInventoryList();
+    
+  };
 
   openAddInventoryForm() {
-    this._dialog.open(InventoryFormComponent)
+    this._dialog.open(InventoryFormComponent) //This will open the form dialog box
+  };
+
+  getInventoryList() {
+    this._invService.getInventoryList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: console.log
+    })
+  };
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
-}
+};
